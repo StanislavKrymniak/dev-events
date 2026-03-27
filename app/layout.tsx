6d342988614ea7f00647c36303e50fbcba7ac5b5
Stanislav Krymniak <kryminak.stas@gmail.com>
2026-03-27 13:49:13 +0100

    implement posthog

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils";
  import React from "react";
  import LightRays from "@/components/LightRays";
  import Navbar from "@/components/Navbar";
+import { PostHogProvider } from "./providers";
 
 const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
@@ -34,26 +35,28 @@ export default function RootLayout({
       className={cn("min-h-screen", "antialiased", schibstedGrotesk.variable, martianMono.variable, "font-sans", geist.variable)}
     >
       <body className="min-h-full flex flex-col">
-      <Navbar />
-        <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
-          <LightRays
-              raysOrigin="top-center-offset"
-              raysColor="#5dfeca"
-              raysSpeed={0.5}
-              lightSpread={0.9}
-              rayLength={1.4}
-              followMouse={true}
-              mouseInfluence={0.02}
-              noiseAmount={0}
-              distortion={0.01}
-              pulsating={false}
-              fadeDistance={1}
-              saturation={1}
-          />
-        </div>
-        <main>
-          {children}
-        </main>
+        <PostHogProvider>
+        <Navbar />
+          <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
+            <LightRays
+                raysOrigin="top-center-offset"
+                raysColor="#5dfeca"
+                raysSpeed={0.5}
+                lightSpread={0.9}
+                rayLength={1.4}
+                followMouse={true}
+                mouseInfluence={0.02}
+                noiseAmount={0}
+                distortion={0.01}
+                pulsating={false}
+                fadeDistance={1}
+                saturation={1}
+            />
+          </div>
+          <main>
+            {children}
+          </main>
+        </PostHogProvider>
       </body>
     </html>
   );
