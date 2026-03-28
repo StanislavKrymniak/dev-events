169eba38655a2d9d312f992a7f8bcf71a08d35f3
Stanislav Krymniak <kryminak.stas@gmail.com>
2026-03-28 14:35:45 +0100

    Commiting changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,13 @@
 import Link from "next/link";
 import Image from 'next/image';
 
-
+// NavBar
 const Navbar = () => {
     return (
         <header>
             <nav>
                 <Link href='/' className="logo" >
                     <Image src="/icons/logo.png" alt="logo" width={24} height={24}/>
-
                     <p>DevEvent</p>
                 </Link>
 
